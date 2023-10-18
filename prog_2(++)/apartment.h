10ce24818e0a1b5824d67398350b5a8f8bb69b81
tatarin <konovalovdan22072004@mail.ru>
2023-10-18 23:08:35 +0700

    Добавлен класс "Собвственник"

--- prog_2(++)/apartment.h
+++ prog_2(++)/apartment.h
@@ -1,2 +1,21 @@
 #pragma once
-#
+#include <string>
+#include <vector>
+
+namespace apartment {
+
+	class Owner {
+	private:
+		std::string fio;
+		std::string name;
+
+	public:
+		Owner(const std::string& fio, std::string& name);
+		std::string GetFIO() const;
+		std::string GetNAME() const;
+	};
+
+
+
+}
+
